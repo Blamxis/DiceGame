554022414036f593360862b2afc9c4526fa7a225
Blamxis <gavinetm26@gmail.com>
2023-12-15 22:56:55 +0100

    Add init function (initialisation du jeu + parametres

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -7,3 +7,26 @@ const newGameBtn = document.querySelector(".new");
 // Variables
 let scores, roundScore, activePlayer, gamePlaying;
 let diceRolled = false;
+
+// Initialise le jeu
+function initGame() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true;
+    diceRolled = false;
+  
+    // Réinitialiser le dé à la face 1
+    dice.style.transform = "rotateX(0deg) rotateY(0deg)";
+  
+  
+    // Réinitialiser l'affichage du score et du score actuel pour chaque joueur
+    document.querySelector("#current-0").textContent = '0';
+    document.querySelector("#current-1").textContent = '0';
+    document.querySelector("#score-0").textContent = '0';
+    document.querySelector("#score-1").textContent = '0';
+  
+    document.querySelector(".panel_1").classList.add('active');
+    document.querySelector(".panel_2").classList.remove('active');
+  
+  }
