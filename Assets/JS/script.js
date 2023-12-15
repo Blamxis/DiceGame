ebe5c7ef1a23a45c9c500e274c76f90e6d5159a3
Blamxis <gavinetm26@gmail.com>
2023-12-15 22:58:46 +0100

    Add random dice function + parametre

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -10,23 +10,43 @@ let diceRolled = false;
 
 // Initialise le jeu
 function initGame() {
-    scores = [0, 0];
-    roundScore = 0;
-    activePlayer = 0;
-    gamePlaying = true;
-    diceRolled = false;
-  
-    // Réinitialiser le dé à la face 1
-    dice.style.transform = "rotateX(0deg) rotateY(0deg)";
-  
-  
-    // Réinitialiser l'affichage du score et du score actuel pour chaque joueur
-    document.querySelector("#current-0").textContent = '0';
-    document.querySelector("#current-1").textContent = '0';
-    document.querySelector("#score-0").textContent = '0';
-    document.querySelector("#score-1").textContent = '0';
-  
-    document.querySelector(".panel_1").classList.add('active');
-    document.querySelector(".panel_2").classList.remove('active');
-  
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+  diceRolled = false;
+
+  // Réinitialiser le dé à la face 1
+  dice.style.transform = "rotateX(0deg) rotateY(0deg)";
+
+  // Réinitialiser l'affichage du score et du score actuel pour chaque joueur
+  document.querySelector("#current-0").textContent = "0";
+  document.querySelector("#current-1").textContent = "0";
+  document.querySelector("#score-0").textContent = "0";
+  document.querySelector("#score-1").textContent = "0";
+
+  document.querySelector(".panel_1").classList.add("active");
+  document.querySelector(".panel_2").classList.remove("active");
+}
+
+// Lance un dé aléatoire et met à jour le score
+function randomDice() {
+  if (gamePlaying) {
+    let diceNumber = Math.floor(Math.random() * 6) + 1;
+    dice.style.animation = "rolling 6s";
+
+    setTimeout(() => {
+      setDiceFace(diceNumber);
+      dice.style.animation = "none";
+      if (diceNumber !== 1) {
+        roundScore += diceNumber;
+        document.querySelector("#current-" + activePlayer).textContent = roundScore;
+      } else {
+        document.querySelector("#current-" + activePlayer).textContent = "0";
+        nextPlayer();
+      }
+    }, 6050);
+
+    diceRolled = true;
   }
+}
